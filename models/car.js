fffd95b06b2eb1c186ebe7b2a321c3bbb5665a6d
kushal-dreamspring <kkhare@dreamspring.org>
2023-03-01 13:13:08 +0530

    Add Models - Add Car model with method to get car number - Add Parking Slot model parking and unpark methods

---
+++ models/car.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(registration_number) {
+    this.registration_number = registration_number;
+  }
+
+  getRegistrationNumber() {
+    return this.registration_number;
+  }
+}
+
+module.exports = Car;
---
+++ models/parking-slot.js
@@ -0,0 +1,38 @@
+const Car = require("./car");
+
+class ParkingSlot {
+  constructor(slot_no) {
+    this.car = null;
+    this.slot_no = slot_no;
+    this.timestamp = 0;
+  }
+
+  isEmpty() {
+    return this.car === null;
+  }
+
+  getCar() {
+    return this.car;
+  }
+
+  getSlotNumber() {
+    return this.slot_no;
+  }
+
+  getTimestamp() {
+    return this.timestamp;
+  }
+
+  parkCar(registration_number, timestamp) {
+    this.car = new Car(registration_number);
+    this.timestamp = timestamp;
+  }
+
+  unparkCar() {
+    delete this.car;
+    this.car = null;
+    this.timestamp = 0;
+  }
+}
+
+module.exports = ParkingSlot;
